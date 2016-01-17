8f20ec92fc7e1a75b08658fead163c3c50ce4658
Christian Hanne <support@aureola.codes>
2016-01-17 18:32:47 +0100

    Added placeholders for custom post types.

---
+++ post-types/match.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains the class of the custom 'Match' post type.
+ *
+ * @author Christian Hanne <support@aureola.codes>
+ * @package Clanpress
+ */
+
+/**
+ * @class Clanpress_Match_Post_Type
+ */
+class Clanpress_Match_Post_Type extends Clanpress_Post_Type {
+
+}
---
+++ post-types/sponsor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains the class of the custom 'Sponsor' post type.
+ *
+ * @author Christian Hanne <support@aureola.codes>
+ * @package Clanpress
+ */
+
+/**
+ * @class Clanpress_Sponsor_Post_Type
+ */
+class Clanpress_Sponsor_Post_Type extends Clanpress_Post_Type {
+
+}
