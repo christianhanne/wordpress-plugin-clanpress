294af737e38c13a1839713ae7b09ddd0ca3e8f63
Christian Hanne <support@aureola.codes>
2016-01-17 19:38:39 +0100

    Refactored & commented form helper class.

--- classes/form.php
+++ classes/form.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * TODO
+ * Contains a helper class for form elements.
  *
  * @author Christian Hanne <support@aureola.codes>
  * @package Clanpress
@@ -11,127 +11,71 @@
  * @class Clanpress_Form
  */
 class Clanpress_Form {
-
   /**
-   * TODO
-   */
-  public static function input($id, $name, $type = 'text', $value, $attributes = array()) {
-    $attributes['id'] = $id;
-    $attributes['name'] = $name;
-    $attributes['type'] = $type;
-    $attributes['value'] = $value;
-
-    return sprintf('<input%s />', self::attributes($attributes));
-  }
-
-  /**
-   * TODO
-   */
-  public static function checkbox($id, $name, $checked, $attributes = array()) {
-    $attributes['class'] = 'checkbox';
-    if ($checked) {
-      $attributes['checked'] = 'checked';
-    }
-
-    return self::input($id, $name, 'checkbox', '1', $attributes);
-  }
-
-  /**
-   * TODO
-   */
-  public static function select($id, $name, $value, $options, $attributes = array()) {
-    $attributes['id'] = $id;
-    $attributes['name'] = $name;
-
-    if (is_array($value)) {
-      $attributes['multiple'] = 'multiple';
-    }
-
-    return vsprintf('<select%s>%s</select>', array(
-      self::attributes($attributes),
-      self::options($options, $value),
-    ));
-  }
-
-  /**
-   * TODO
-   */
-  public static function label($id, $title) {
-    return sprintf('<label for="%s">%s:</label>', $id, $title);
-  }
-
-  /**
-   * TODO
+   * Returns a form element of the given type.
+   *
+   * @param array $element
+   *   Contains the form element definition as an associative array. The
+   *   following fields will be processed (optional fields are marked as such):
+   *   - type:
+   *     Defines the form element's type.
+   *     Allowed values: input, number, checkbox, select
+   *   - field_id:
+   *     The form element's id as defined by wordpress.
+   *   - field_name:
+   *     The form element's name as defined by wordpress.
+   *   - label: (optional)
+   *     Will create a label element with the given value as the content
+   *     above the actual form element.
+   *   - options:
+   *     This is only required for elements with options (like select). Should
+   *     contain an array of option values and labels. Eg. foo => bar will
+   *     output <option="foo">bar</option> for selects.
+   *   - value: (optional)
+   *     The form element's current value.
+   *   - default: (optional)
+   *     The form element's default value. If the element does not have a value
+   *     the value of this field will be used instead.
+   *   - attributes:
+   *     An associative array of html attributes. Should be defined as
+   *     attribute => value (will output <* attribute="value">) or
+   *     attribute => bool (will output <* attribute>).
+   *
+   * @return string
+   *   Html markup of a form element.
    */
   public static function element($element) {
-    $element_value = isset( $element['default'] ) ? $element['default'] : '';
-    if ( !empty( $element['value'] ) ) {
-      $element_value = esc_attr( $element['value'] );
+    if ( empty( $element['value'] ) ) {
+      $element['value'] = isset( $element['default'] ) ? $element['default'] : '';
+    }
+    else {
+      $element['value'] = esc_attr( $element['value'] );
     }
 
-    $element_label = '';
+    $label = '';
     if ( isset( $element['label'] ) ) {
-      $element_label = self::label( $element['field_id'], $element['label'] );
+      $label = self::label( $element['field_id'], $element['label'] );
     }
 
-    $element_options = isset( $element['options'] ) ? (array) $element['options'] : array();
-    $element_attributes = isset( $element['attributes'] ) ? (array) $element['attributes'] : array();
+    $element['options'] = isset( $element['options'] ) ? (array) $element['options'] : array();
+    $element['attributes'] = isset( $element['attributes'] ) ? (array) $element['attributes'] : array();
 
     switch ($element['type']) {
       case 'select':
-        $element_attributes['class'] = 'widefat';
-
-        return vsprintf('<p>%s%s</p>', array(
-          $element_label,
-          self::select(
-            $element['field_id'],
-            $element['field_name'],
-            $element_value,
-            $element_options,
-            $element_attributes
-          ),
-        ));
+        $element['attributes']['class'] = 'widefat';
+        return sprintf( '<p>%s%s</p>', $label, self::select($element) );
 
       case 'checkbox':
-        $element_attributes['class'] = 'widefat';
-
-        return vsprintf('<p>%s%s</p>', array(
-          self::checkbox(
-            $element['field_id'],
-            $element['field_name'],
-            $element_value,
-            $element_attributes
-          ),
-          $element_label,
-        ));
+        $element['attributes']['class'] = 'widefat';
+        return sprintf( '<p>%s%s</p>', self::checkbox($element), $label );
 
       case 'number':
-        $element_attributes['class'] = 'tiny-text';
-
-        return vsprintf('<p>%s %s</p>', array(
-          $element_label,
-          self::input(
-            $element['field_id'],
-            $element['field_name'],
-            $element['type'],
-            $element_value,
-            $element_attributes
-          ),
-        ));
+        $element['attributes']['class'] = 'tiny-text';
+        return sprintf( '<p>%s %s</p>', $label, self::input($element) );
 
       case 'text':
-        $element_attributes['class'] = 'widefat';
-
-        return vsprintf('<p>%s%s</p>', array(
-          $element_label,
-          self::input(
-            $element['field_id'],
-            $element['field_name'],
-            $element['type'],
-            $element_value,
-            $element_attributes
-          ),
-        ));
+        $element['attributes']['class'] = 'widefat';
+        return sprintf( '<p>%s%s</p>', $label, self::input($element) );
 
       default:
         return '';
@@ -139,7 +83,100 @@ class Clanpress_Form {
   }
 
   /**
-   * TODO
+   * Returns the html markup of an input form element.
+   *
+   * @param array $element
+   *   Contains the form element definition as an associative array.
+   *   Please check the linked method for further details.
+   *
+   * @return string
+   *   Html markup of an input form element.
+   *
+   * @see Clanpress_Form::element()
+   */
+  private static function input($element) {
+    $attributes = isset( $element['attributes'] ) ? $element['attributes'] : array();
+    $attributes['id'] = $element['field_id'];
+    $attributes['name'] = $element['field_name'];
+    $attributes['type'] = $element['type'];
+    $attributes['value'] = $element['value'];
+    return sprintf('<input%s />', self::attributes($attributes));
+  }
+
+  /**
+   * Returns the html markup of a checkbox form element.
+   *
+   * @param array $element
+   *   Contains the form element definition as an associative array.
+   *   Please check the linked method for further details.
+   *
+   * @return string
+   *   Html markup of a checkbox form element.
+   *
+   * @see Clanpress_Form::input()
+   * @see Clanpress_Form::element()
+   */
+  private static function checkbox($element) {
+    $element['attributes']['value'] = 1;
+    $element['attributes']['class'] = 'checkbox';
+    if ($element['value'] == $element['attributes']['value']) {
+      $element['attributes']['checked'] = 'checked';
+    }
+
+    return self::input($element);
+  }
+
+  /**
+   * Returns the html markup of a select form element.
+   *
+   * @param array $element
+   *   Contains the form element definition as an associative array.
+   *   Please check the linked method for further details.
+   *
+   * @return string
+   *   Html markup of a select form element.
+   *
+   * @see Clanpress_Form::element()
+   */
+  private static function select($element) {
+    $element['attributes']['id'] = $element['field_id'];
+    $element['attributes']['name'] = $element['field_name'];;
+
+    if (is_array($element['value'])) {
+      $element['attributes']['multiple'] = 'multiple';
+    }
+
+    return vsprintf('<select%s>%s</select>', array(
+      self::attributes($element['attributes']),
+      self::options($element['options'], $element['value']),
+    ));
+  }
+
+  /**
+   * Returns the html markup of a label element.
+   *
+   * @param string $id
+   *   Html id of the label's form field.
+   * @param string $title
+   *   The title this label contain.
+   *
+   * @return string
+   *   Html markup of a label element.
+   */
+  private static function label($id, $title) {
+    return sprintf('<label for="%s">%s:</label>', $id, $title);
+  }
+
+  /**
+   * Returns an attributes string for use in html elements.
+   *
+   * @param array $attributes
+   *   An associative array of html attributes. Should be defined as
+   *   attribute => value (will output <* attribute="value">) or
+   *   attribute => bool (will output <* attribute>).
+   *
+   * @return string
+   *   Html attributes string.
    */
   private static function attributes($attributes = array()) {
     $output = '';
@@ -156,7 +193,19 @@ class Clanpress_Form {
   }
 
   /**
-   * TODO
+   * Returns a select options string for an array of options.
+   *
+   * @param array $options
+   *   Should contain an array of option values and labels. Eg. foo => bar will
+   *   output <option="foo">bar</option> for selects.
+   * @param string|array $value
+   *   The value of the corresponding form element. Will be used to
+   *   mark options as selected.
+   *
+   * @return string
+   *   Html output of select options.
+   *
+   * @see Clanpress::select()
    */
   private static function options($options, $value) {
     $output = '';
