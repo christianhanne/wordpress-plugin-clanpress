66c8be0c3a89b469f9e9fd0170b604ccd27dcc4b
Christian Hanne <support@aureola.codes>
2016-01-23 22:23:32 +0100

    Added first draft of latest matches list.

--- templates/widgets/widget-clanpress-latest-matches.php
+++ templates/widgets/widget-clanpress-latest-matches.php
@@ -1,1 +1,7 @@
-TODO
+<ul class="latest_matches">
+  <?php foreach ($links as $link): ?>
+    <li class="latest_matches__item latest_matches__item--<?php echo $link['id']; ?>">
+      <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
+    </li>
+  <?php endforeach; ?>
+</ul>
