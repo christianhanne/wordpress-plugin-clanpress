64c0ff54626d62efbaa5f494d3e03b98eadc40f1
Christian Hanne <support@aureola.codes>
2016-01-23 23:00:39 +0100

    Added first draft of sponsors list.

--- templates/widgets/widget-clanpress-sponsors.php
+++ templates/widgets/widget-clanpress-sponsors.php
@@ -1,1 +1,7 @@
-TODO
+<ul class="sponsors">
+  <?php foreach ($links as $link): ?>
+    <li class="sponsors__item sponsors__item--<?php echo $link['id']; ?>">
+      <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
+    </li>
+  <?php endforeach; ?>
+</ul>
