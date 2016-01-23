fa694895ab67b89d15ce186bb097f570579d073d
Christian Hanne <support@aureola.codes>
2016-01-23 22:21:24 +0100

    Added first draft of latest awards list.

--- templates/widgets/widget-clanpress-latest-awards.php
+++ templates/widgets/widget-clanpress-latest-awards.php
@@ -1,1 +1,7 @@
-TODO
+<ul class="latest_awards">
+  <?php foreach ($links as $link): ?>
+    <li class="latest_awards__item latest_awards__item--<?php echo $link['id']; ?>">
+      <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
+    </li>
+  <?php endforeach; ?>
+</ul>
