b024d43747ed817fdfada84fdf10850e66fac814
Christian Hanne <support@aureola.codes>
2016-01-24 15:23:32 +0100

    Updated version number.

--- clanpress.php
+++ clanpress.php
@@ -13,13 +13,13 @@ Plugin URI: http://aureola.codes/wordpress-plugins/clanpress
 Description: TODO
 Author: Christian Hanne <support@aureola.codes>
 Author URI: http://aureola.codes/wordpress-plugins/
-Version: 0.1.1
+Version: 0.1.2
 Text Domain: clanpress
 */
 
 defined( 'ABSPATH' ) or die( 'Access restricted.' );
 
-define( 'CLANPRESS_VERSION', '0.1.1' );
+define( 'CLANPRESS_VERSION', '0.1.2' );
 define( 'CLANPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CLANPRESS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
