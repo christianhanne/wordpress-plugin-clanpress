02c5d2f5ad3fb17ccf4a1b409018645eba2faf8b
Christian Hanne <support@aureola.codes>
2016-02-01 23:24:54 +0100

    Compiled sources.

--- dist/js/upload-field.js
+++ dist/js/upload-field.js
@@ -15,18 +15,33 @@
       var $field = $(this);
       var $images = $field.parent().find('.clanpress-upload-image');
       var $button = $field.parent().find('.clanpress-upload-button');
+      var $description = $field.parent().find('.description');
 
       $button.click(function _selectImage(e) {
         e.preventDefault();
 
-        wp.media.editor.send.attachment = function (props, attachment) {
+        var uploader = undefined,
+            options = {
+          button: {
+            text: $button.attr('value')
+          },
+          multiple: false
+        };
+
+        if ($description.size() > 0) {
+          options.title = $description.text();
+        }
+
+        uploader = wp.media(options);
+        uploader.on('select', function _onSelect() {
+          var attachment = uploader.state().get('selection').first().toJSON();
           var previewUrl = attachment.sizes.thumbnail.url;
           $images.html('<img src="' + previewUrl + '" alt="Preview" />');
 
           $field.val(attachment.id);
-        };
+        });
 
-        wp.media.editor.open(this);
+        uploader.open();
       });
     });
   });
