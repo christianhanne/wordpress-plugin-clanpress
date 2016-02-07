3a9ae389b932440fb6b3608eb0fe6ee6063e67c8
Christian Hanne <support@aureola.codes>
2016-02-07 22:47:27 +0100

    Added public functions for clanpress awards.

--- includes/helper/functions.php
+++ includes/helper/functions.php
@@ -198,3 +198,39 @@ function clanpress_the_sponsor_link($post = null) {
     __( 'To the sponsor', 'clanpress' ),
   ));
 }
+
+/**
+ * Displays the award's placement.
+ *
+ * @param WP_Post $post
+ *   The post.
+ */
+function clanpress_the_award_placement($post = null) {
+  $post = isset( $post ) ? $post : get_post();
+
+  $placement = get_post_meta( $post->ID, 'clanpress_award_placement[placement]', true );
+  echo !empty( $placement ) ? $placement : '&ndash;';
+}
+
+/**
+ * Display the award's squads.
+ *
+ * @param WP_Post $post
+ *   The post.
+ */
+function clanpress_the_award_squad($post = null) {
+  $post = isset( $post ) ? $post : get_post();
+
+  $squad_options = Clanpress_Helper::get_squad_options();
+
+  $squads = array();
+  $squad_ids = get_post_meta( $post->ID, 'clanpress_award_squad[squad]', true );
+
+  if (is_array($squad_ids)) {
+    foreach ( $squad_ids as $squad_id => $checked ) {
+      array_push( $squads, $squad_options[ $squad_id ] );
+    }
+  }
+
+  echo implode(', ', $squads);
+}
