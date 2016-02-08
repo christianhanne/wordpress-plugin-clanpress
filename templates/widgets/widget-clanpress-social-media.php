55f19aa1abe343c4ef6a5713f4bebdfb399efd53
Christian Hanne <support@aureola.codes>
2016-02-08 22:25:41 +0100

    Finished social media widget template.

--- templates/widgets/widget-clanpress-social-media.php
+++ templates/widgets/widget-clanpress-social-media.php
@@ -1,6 +1,15 @@
-<ul class="social_media">
+<?php
+/**
+ * @file
+ * Template of the social media widget.
+ *
+ * @author Christian Hanne <support @aureola.codes>
+ * @package Clanpress
+ */
+?>
+<ul class="clanpress_widget__items">
   <?php foreach ($links as $key => $link): ?>
-    <li class="social_media__item social_media__item--<?php echo $key; ?>">
+    <li class="clanpress_widget__item clanpress_widget__item--<?php echo $key; ?>">
       <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
     </li>
   <?php endforeach; ?>
