c21d2aea4542a9e103ce01b33e544b4bc4ce9420
Christian Hanne <support@aureola.codes>
2016-02-08 22:33:29 +0100

    Finished squads widget template.

--- templates/widgets/widget-clanpress-squads.php
+++ templates/widgets/widget-clanpress-squads.php
@@ -1,7 +1,28 @@
-<ul class="squads">
-  <?php foreach ($links as $link): ?>
-    <li class="squads__item squads__item--<?php echo $link['id']; ?>">
-      <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
-    </li>
-  <?php endforeach; ?>
-</ul>
+<?php
+/**
+ * @file
+ * Template of the squads widget.
+ *
+ * @author Christian Hanne <support @aureola.codes>
+ * @package Clanpress
+ */
+?>
+<?php if ( have_posts() ): ?>
+  <ul class="clanpress_widget__items">
+    <?php while(have_posts()) : the_post(); ?>
+      <li class="clanpress_widget__item">
+        <span class="clanpress_widget__thumbnail">
+          <?php the_post_thumbnail( 'thumbnail' ); ?>
+        </span>
+        <h4 class="clanpress_widget__title">
+          <?php the_title(); ?>
+        </h4>
+        <a href="<?php the_permalink(); ?>">
+          <?php _e( 'View Squad', 'clanpress_the_award_placement' ); ?>
+        </a>
+      </li>
+    <?php endwhile; ?>
+  </ul>
+<?php else: ?>
+  <?php _e( 'No squads found.', 'clanpress' ); ?>
+<?php endif; ?>
