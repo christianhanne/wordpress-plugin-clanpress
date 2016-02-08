4b5962a1539d9d42fc8b5ed78139696e121615a1
Christian Hanne <support@aureola.codes>
2016-02-08 22:29:27 +0100

    Finished sponsors widget template.

--- templates/widgets/widget-clanpress-sponsors.php
+++ templates/widgets/widget-clanpress-sponsors.php
@@ -1,7 +1,22 @@
-<ul class="sponsors">
-  <?php foreach ($links as $link): ?>
-    <li class="sponsors__item sponsors__item--<?php echo $link['id']; ?>">
-      <a href="<?php echo $link['href']; ?>"><?php echo $link['title']; ?></a>
-    </li>
-  <?php endforeach; ?>
-</ul>
+<?php
+/**
+ * @file
+ * Template of the sponsors widget.
+ *
+ * @author Christian Hanne <support @aureola.codes>
+ * @package Clanpress
+ */
+?>
+<?php if ( have_posts() ): ?>
+  <ul class="clanpress_widget__items">
+    <?php while(have_posts()) : the_post(); ?>
+      <li class="clanpress_widget__item">
+        <a href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail( 'thumbnail' ); ?>
+        </a>
+      </li>
+    <?php endwhile; ?>
+  </ul>
+<?php else: ?>
+  <?php _e( 'No sponsors found.', 'clanpress' ); ?>
+<?php endif; ?>
