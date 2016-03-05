42c35cc5e0167d6624aa87e4a1d18a36bd2d4f53
Christian Hanne <support@aureola.codes>
2016-03-05 20:29:31 +0100

    Added settings for main component.

--- components/admin/admin.php
+++ components/admin/admin.php
@@ -18,9 +18,62 @@ class Clanpress_Admin_Component extends Clanpress_Component {
   /**
    * @inheritdoc
    */
+  protected function includes() {
+    return array(
+      'functions',
+    );
+  }
+
+  /**
+   * @inheritdoc
+   */
   protected function admin_pages() {
     return array(
       'clanpress',
     );
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function settings() {
+    return array(
+      'clan_name' => array(
+        'type' => 'text',
+        'label' => __( 'Name of the Clan', 'clanpress' ),
+        'pattern' => '.+',
+      ),
+      'clan_subtitle' => array(
+        'type' => 'text',
+        'label' => __( 'Subtitle', 'clanpress' ),
+        'pattern' => '.+',
+      ),
+      'logo' => array(
+        'type' => 'upload',
+        'label' => __( 'Logo', 'clanpress' ),
+      ),
+      'icon' => array(
+        'type' => 'upload',
+        'label' => __( 'Icon', 'clanpress' ),
+      ),
+      'header' => array(
+        'type' => 'upload',
+        'label' => __( 'Header', 'clanpress' ),
+      ),
+      'footer' => array(
+        'type' => 'textarea',
+        'label' => __( 'Footer', 'clanpress' ),
+        'attributes' => array(
+          'rows' => 5,
+        ),
+      ),
+    );
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function name() {
+    return __( 'Clanpress', 'clanpress' );
   }
 }
---
+++ components/admin/includes/functions.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Contains publicly accessible functions for this component.
+ *
+ * @author Christian Hanne <support@aureola.codes>
+ * @copyright Copyright (c) 2016, Aureola
+ * @license https://github.com/aureolacodes/clanpress/blob/master/LICENSE
+ *
+ * @package Clanpress
+ */
+
+defined( 'ABSPATH' ) or die( 'Access restricted.' );
+
+/**
+ * Displays the stored clan name.
+ */
+function clanpress_the_clan_name() {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['clan_name'] ) ) {
+    echo esc_html( $settings['clan_name'] );
+  }
+}
+
+/**
+ * Displays the stored clan subtitle.
+ */
+function clanpress_the_subtitle() {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['clan_subtitle'] ) ) {
+    echo esc_html( $settings['clan_subtitle'] );
+  }
+}
+
+/**
+ * Displays a previously stored header image.
+ *
+ * @param string $size
+ *   Size of the image.
+ */
+function clanpress_the_header_image( $size = 'thumbnail' ) {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['header'] ) ) {
+    $attachment = wp_get_attachment_image( $settings['header'], $size );
+  }
+}
+
+/**
+ * Displays a previously stored icon image.
+ *
+ * @param string $size
+ *   Size of the image.
+ */
+function clanpress_the_icon_image( $type = 'thumbnail' ) {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['icon'] ) ) {
+    $attachment = wp_get_attachment_image( $settings['icon'], $size );
+  }
+}
+
+/**
+ * Displays a previously stored logo image.
+ *
+ * @param string $size
+ *   Size of the image.
+ */
+function clanpress_the_logo_image( $type = 'thumbnail' ) {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['logo'] ) ) {
+    $attachment = wp_get_attachment_image( $settings['logo'], $size );
+  }
+}
+
+/**
+ * Displays the stored footer.
+ */
+function clanpress_the_footer() {
+  $settings = Clanpress_Settings::instance()->get_values( 'admin' );
+  if ( !empty( $settings['footer'] ) ) {
+    echo nl2br( esc_html( $settings['footer'] ) );
+  }
+}
