2b663ad0fe2ba258771d2c758a2c6dfa937e8000
Christian Hanne <support@aureola.codes>
2016-04-24 18:55:14 +0200

    Added svg as allowed mime type.

--- components/shared/shared.php
+++ components/shared/shared.php
@@ -15,6 +15,30 @@ defined( 'ABSPATH' ) or die( 'Access restricted.' );
  * @{inheritdoc}
  */
 class Clanpress_Shared_Component extends Clanpress_Component {
+
+  /**
+   * Setup clanpress shared component.
+   */
+  public function __construct() {
+    parent::__construct();
+
+    add_filter('upload_mimes', array( $this, 'upload_mimes' ) );
+  }
+
+  /**
+   * Update allowed mime types.
+   *
+   * @param array $mimes
+   *   Original array of mime types.
+   *
+   * @return mixed
+   *   Array of mime types.
+   */
+  public function upload_mimes( $mimes ) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+
   /**
    * @{inheritdoc}
    */
